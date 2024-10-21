4b5385a77a3b397fd4e409ff2306d922888dee30
Aurumdev952 <benjamugi20072@gmail.com>
2024-10-21 10:36:38 +0200

    add authentication

--- donation_tracking/urls.py
+++ donation_tracking/urls.py
@@ -18,10 +18,17 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
+
+from users import views as user_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("signup/", user_views.signup, name="signup"),
+    path("login/", user_views.login, name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import SignUpForm
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if form.cleaned_data["phone"]:
+                user.profile.phone = form.cleaned_data["phone"]
+                user.profile.save()
+            auth_login(request, user)
+            return redirect("/")
+    else:
+        form = SignUpForm()
+    return render(request, "signup.html", {"form": form})
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect("/")
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
